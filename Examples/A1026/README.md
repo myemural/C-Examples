34f97217b519dec4bf751f036a1ee0448db7fd05
myemural <mustafayemural@gmail.com>
2022-08-17 23:17:12 +0300

    Added: A1026

---
+++ Examples/A1026/README.md
@@ -0,0 +1,8 @@
+**Örnek Adı:** While Örneği 1 <br>
+**Yazan:** Mustafa Yemural <br>
+**Örneğin Seviyesi:** :large_blue_circle: <br>
+# Açıklama #
+<p>Bu örnekte C'de while kullanımı gösterilmiştir.</p>
+
+# Notlar #
+- 
---
+++ Examples/A1026/main.c
@@ -0,0 +1,20 @@
+/*
+* Author: Mustafa Yemural
+* Date: 06/02/2017
+* Description: While Örneði 1
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int i=0;
+    
+    while(i<10) // Þartlý döngüyü belirtir.
+    {
+    printf("%d\n",i);
+    i++;      
+    }
+    
+    return 0;
+}
