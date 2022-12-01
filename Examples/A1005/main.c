81c4e8ab4af2bbcd55b055ba7629bf67fa121bfb
myemural <mustafayemural@gmail.com>
2022-12-01 23:14:31 +0300

    Updated: A1005

--- Examples/A1005/main.c
+++ Examples/A1005/main.c
@@ -1,19 +1,22 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: Tür Dönüşümü Örneği 1
-*/
+/** @file main.c
+ *  @brief Type Conversion Example 1
+ *
+ *  This shows promotion.
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
 main()
 {
-      int a=5;
-      double b=7.2,sonuc;
+      int a = 5;
+      double b = 7.2, sonuc;
 
-      sonuc=a+b; // Küçük olan tür, büyük olan türe dönüştürülür. Bu duruma terfi(promotion) denir.
+      sonuc = a + b; /* Little type is converted into big type and this called promotion. */
 
-      printf("%f\n",sonuc);
+      printf("%f\n", sonuc);
 
       return 0;
 }
