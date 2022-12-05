e551ed59b13ba36ab1e8e34152c1c7f96baf10f9
myemural <mustafayemural@gmail.com>
2022-12-05 21:31:34 +0300

    Updated: A1007

--- Examples/A1007/main.c
+++ Examples/A1007/main.c
@@ -1,21 +1,24 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: Fonksiyon Çağırmalarında Tür Dönüşümü
-*/
+/** @file main.c
+ *  @brief Type Conversion on Function Calls
+ *
+ *  Type Conversion on Function Calls
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
-double carp(double x, double y)
+double mult(double x, double y)
 {
        return x*y;
 }
 
 int main()
 {
-      int a=6,b=5;
+      int a = 6, b = 5;
       
-      printf("%f\n",carp(a,b)); // "int" türü "double" türüne dönüştürülür. Bu dönüşüme "Fonksiyon Çağırmalarında Tür Dönüşümü" denir.
+      printf("%f\n", mult(a,b)); /* int converted to double */
       
       return 0;
 }
