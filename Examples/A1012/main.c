6a181c5b827bfc8e1a2a39efb9d12b8971b1969c
myemural <mustafayemural@gmail.com>
2022-12-13 22:34:46 +0300

    Updated: A1012

--- Examples/A1012/main.c
+++ Examples/A1012/main.c
@@ -1,20 +1,23 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: printf Tür Karakterleri
-*/
+/** @file main.c
+ *  @brief printf Format Specifiers
+ *
+ *  printf Format Specifiers
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include <stdio.h>
 
 main()
 {
-    char a='A';
-    short b=32000;
-    int c=234;
-    long d=2000000000;
-    float e=1.34534;
-    double f=12.124312;
-    long double g=-30.2134123412;
+    char a= 'A';
+    short b= 32000;
+    int c= 234;
+    long d= 2000000000;
+    float e= 1.34534;
+    double f= 12.124312;
+    long double g= -30.2134123412;
     
     printf("%c \n", a);
     printf("%hd \n", b);
@@ -26,18 +29,3 @@ main()
     
     return 0;      
 }
-/*
-%d: "int" türünü desimal sistemde yazar.
-%ld: "long" türünü desimal sistemde yazar
-%x: "unsigned int" türünü hexadecimal sistemde yazar.
-%X:	"unsigned int" türünü hexadecimal sistemde yazar.(semboller büyük harfle)
-%lx: "unsigned long" türünü hexadecimal sistemde yazar.
-%u: "unsigned int" türünü decimal sistemde yazar.
-%o: "unsigned int" türünü oktal sistemde yazar.
-%f: "float" ve "double" türlerini desimal sistemde yazar.
-%lf: "double" türünü desimal sistemde yazar.
-%e: gerçek sayýlarý üstel biçimde yazar.
-%c: "char" veya "int" türünü karakter görüntüsü olarak yazdýrýr.
-%s: "string" olarak yazdýrýr.
-%lf: "long double" türünü desimal sistemde yazdýrýr.
-*/
