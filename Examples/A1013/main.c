4252590131a3df18905c67b859178488b7b18524
myemural <mustafayemural@gmail.com>
2022-12-14 22:48:45 +0300

    Updated: A1013

--- Examples/A1013/main.c
+++ Examples/A1013/main.c
@@ -1,17 +1,20 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: Bir Karakterin ASCII Karşılığı (ASCII Value of a Character)
-*/
+/** @file main.c
+ *  @brief ASCII Value of a Character
+ *
+ *  ASCII Value of a Character
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
 int main()
 {
 	char c;
-	printf("Bir karakter girin: "); // Kullanıcıya gönderilen istem(prompt) mesjıdır.
-	scanf("%c", &c); // Kullanıcıdan karakter tipinde bir veri alınır.
-	printf("%c karakterinin ASCII karsiligi: %d", c, c); // %d ile karakterin ASCII karşılığı gösterilir.
+	printf("Bir karakter girin: ");
+	scanf("%c", &c);
+	printf("%c karakterinin ASCII karsiligi: %d", c, c);
 
 	return 0;
 }
