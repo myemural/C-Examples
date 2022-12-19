0b0e40557c88a6f77581f6d221ab18b205ec023b
myemural <mustafayemural@gmail.com>
2022-12-19 22:26:43 +0300

    Updated: A1017

--- Examples/A1017/main.c
+++ Examples/A1017/main.c
@@ -1,23 +1,27 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: isalpha Fonksiyonu
-*/
+/** @file main.c
+ *  @brief isalpha Function
+ *
+ *  isalpha Function
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
-#include<ctype.h> // Bazý standart C fonksiyonlarýný içerir.
+#include<ctype.h>
 
 main()
 {
-      char ch;
+    char ch;
       
-      printf("Bir karakter girin: ");
-      ch=getchar(); // Karakter giriþi...
+    printf("Bir karakter girin: ");
+    ch = getchar();
       
-      if(isalpha(ch)) // "isalpha" fonksiyonu girilen karakterin büyük veya küçük bir harf karakteri mi yoksa harfin dýþýnda bir karakter mi olduðunu kontrol eden standart bir C fonksiyonudur. Eðer harf ise 1, deðil ise 0 deðerini döndürür.
-      {printf("\nBu bir harftir...\n");}
-      else
-      {printf("\nBu bir harf degildir...\n");}
+    if(isalpha(ch)) {
+        printf("\nBu bir harftir...\n");
+    } else {
+        printf("\nBu bir harf degildir...\n");
+    }
       
-      return 0;
+    return 0;
 }
