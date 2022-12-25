8481921710e0db4fa9d888bf38de05336554da05
myemural <mustafayemural@gmail.com>
2022-12-25 23:33:23 +0300

    Updated: A1021

--- Examples/A1021/main.c
+++ Examples/A1021/main.c
@@ -1,8 +1,11 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: Karþýlaþtýrma Operatörleri
-*/
+/** @file main.c
+ *  @brief Comparison Operators
+ *
+ *  Comparison Operators
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
@@ -11,19 +14,8 @@ int main()
       int x = 5;
       int y;
       
-      y = (x > 5) + 1; // Buradaki ifade mantýksal olarak doðru(1) veya yanlýþ(0) belirtir.
+      y = (x > 5) + 1;
       printf("%d\n", y);
       
       return 0;
 }
-/*
-KARÞILAÞTIRMA OPERATÖRLERÝ:
-<       küçüktür (less than)
-> 		büyüktür (greater than)
-<=		küçük eþit (less than or equal)
->=		büyük eþit (greater than or equal)
-==		eþittir. (equal)
-!=  	eþit deðildir. (not equal)
-
-NOT: Bu operatörlerin hepsi iki operand alan araek operatörlerdir (binary infix). Hepsi soldan saða doðrudur. "==" ve "!=" ifadeleri 7. seviyededir. Diðerleri 6. seviyededir.
-*/
