07622fda052441a3456917824514128af58cd978
myemural <mustafayemural@gmail.com>
2022-12-26 23:28:22 +0300

    Updated: A1022

--- Examples/A1022/main.c
+++ Examples/A1022/main.c
@@ -1,8 +1,11 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: Mantýksal Operatörler
-*/
+/** @file main.c
+ *  @brief Logical Operators
+ *
+ *  Logical Operators
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
@@ -14,15 +17,3 @@ main()
       
       return 0;
 }
-/*
-MANTIKSAL OPERATÖRLER:
-
-1) "!" Operatörü(Deðil): Deðil operatörü tek operand alýr ve her zaman önek durumundadýr (unary prefix). 2. seviyededir ve saðdan sola doðru ilerler.
-Örnek: x = !25; // x deðiþkenine "0" deðeri atanýr.
-
-2) "&&" Operatörü(Ve): 11. seviyededir ve soldan saða doðru ilerler.
-Örnek: x = 3 < 5 && 7; // x=1
-
-3) "||" Operatörü(Veya): 12. seviyededir ve soldan saða doðru ilerler.
-Örnek: a = 3 || 5; // a=1
-*/
