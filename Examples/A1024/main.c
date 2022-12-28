8bb4b1a4dc59ae8935f62b40f15a029cad8d84de
myemural <mustafayemural@gmail.com>
2022-12-28 22:23:48 +0300

    Updated: A1024

--- Examples/A1024/main.c
+++ Examples/A1024/main.c
@@ -1,40 +1,43 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: Rakam veya Deðil (Digit or Not)
-*/
+/** @file main.c
+ *  @brief Digit or Not
+ *
+ *  Digit or Not
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     printf("Lutfen bir rakam girin: ");
-    
+
     int r;
-    scanf("%d",&r);
-    
-    switch(r)
+    scanf("%d", &r);
+
+    switch (r)
     {
     case 0:
     case 1:
     case 2:
     case 3:
     case 4:
-    case 5: 
+    case 5:
     case 6:
     case 7:
     case 8:
-    case 9: 
-         {
-         printf("Bu bir rakamdir.\n"); // Girilen tüm deðerler için bu kod geçerlidir.
-         break;  
-         }   
-    default:
-         {
-         printf("Belirsiz...\n");
-         break;  
-         } 
+    case 9:
+    {
+        printf("Bu bir rakamdir.\n");
+        break;
     }
-    
+    default:
+    {
+        printf("Belirsiz...\n");
+        break;
+    }
+    }
+
     return 0;
 }
