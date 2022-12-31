d15ab8ccc15e618c9b3450e42b0865c3a1dc3f93
myemural <mustafayemural@gmail.com>
2022-12-31 23:26:46 +0300

    Updated: A1026

--- Examples/A1026/main.c
+++ Examples/A1026/main.c
@@ -1,19 +1,21 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: While Örneði 1
-*/
+/** @file main.c
+ *  @brief while Example 1
+ *
+ *  while Example 1
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
 int main()
 {
-    int i=0;
+    int i = 0;
     
-    while(i<10) // Þartlý döngüyü belirtir.
-    {
-    printf("%d\n",i);
-    i++;      
+    while(i < 10) {
+        printf("%d\n", i);
+        i++;      
     }
     
     return 0;
