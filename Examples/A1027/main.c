f17551438a7296d7287fc13351bf8ad5af097aed
myemural <mustafayemural@gmail.com>
2023-01-01 22:24:50 +0300

    Updated: A1027

--- Examples/A1027/main.c
+++ Examples/A1027/main.c
@@ -1,18 +1,20 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: While Örneði 2
-*/
+/** @file main.c
+ *  @brief while Example 2
+ *
+ *  while Example 2
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
 int main()
 {
-    int i=0;
+    int i = 0;
     
-    while(i++<10) // Bireþik Postfix ifadesi...
-    {
-    printf("%d\n",i);  
+    while (i++ < 10) {
+        printf("%d\n", i);  
     }
     
     return 0;
