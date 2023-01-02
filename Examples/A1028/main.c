a2365fdeb66356f7c9dc5ce293d4c01098e392d4
myemural <mustafayemural@gmail.com>
2023-01-02 21:59:17 +0300

    Updated: A1028

--- Examples/A1028/main.c
+++ Examples/A1028/main.c
@@ -1,19 +1,21 @@
-/*
-* Author: Mustafa Yemural
-* Date: 06/02/2017
-* Description: While Örneði 3
-*/
+/** @file main.c
+ *  @brief while Example 3
+ *
+ *  while Example 3
+ *
+ *  @author Mustafa Yemural (myemural)
+ *  @date 06/02/2017
+ */
 
 #include<stdio.h>
 
 int main()
 {
-    int i=0;
-    int k=5;
+    int i = 0;
+    int k = 5;
     
-    while(k--) // "k" kadar dönen döngü...
-    {
-    printf("%d\n",i);  
+    while(k--) {
+        printf("%d\n", i);  
     }
     
     return 0;
