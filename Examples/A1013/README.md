1d4adbd4a1b137e0b44dad24a0d9536b6c39441f
myemural <mustafayemural@gmail.com>
2023-05-01 23:36:32 +0300

    Added: A1013

--- Examples/A1013/README.md
+++ Examples/A1013/README.md
@@ -1,8 +1,8 @@
-**Örnek Adı:** TBD <br>
+**Örnek Adı:** Bir Sayının Armstrong Sayısı Olup Olmadığını Kontrol Etme (Check Whether a Number is an Armstrong Number) <br>
 **Yazan:** Mustafa Yemural <br>
 **Örneğin Seviyesi:** :large_blue_circle: <br>
 # Açıklama #
-<p>TBD</p>
+<p>Armstrong Sayısı, N rakamlı bir sayının rakamlarının N. kuvvetlerinin toplamı yine o sayıya eşit olan sayılara verilen isimdir. Bu programda kullanıcıdan alınan bir sayının Armstrong sayısı olup olmadığı kontrol edilmektedir.</p>
 
 # Notlar #
-- 
+- Bu örneği düzgün anlayabilmek için [Temel C Eğitiminin](https://www.mustafayemural.com/temel-c-egitimi/) "[C’de for Deyimi ile İlgili Özellikler ve Örnekler](https://www.mustafayemural.com/c-my000028/)" kısmına kadar okunması tavsiye edilir.
--- Examples/A1013/main.c
+++ Examples/A1013/main.c
@@ -1,20 +1,51 @@
 /** @file main.c
- *  @brief ASCII Value of a Character
+ *  @brief Check Whether a Number is an Armstrong Number
  *
- *  ASCII Value of a Character
+ *  This program gets a number from user and checks whether this
+ *  number is an Armstrong Number or not.
  *
  *  @author Mustafa Yemural (myemural)
  *  @date 06/02/2017
  */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	char c;
-	printf("Bir karakter girin: ");
-	scanf("%c", &c);
-	printf("%c karakterinin ASCII karsiligi: %d", c, c);
+    int number;
 
-	return 0;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    int temp = number;
+    int digit_count = 0;
+
+    /* Find digit count */
+    while (temp > 0) {
+        digit_count++;
+        temp /= 10;
+    }
+    
+    int sum = 0;
+    temp = number;
+
+    /* Find summation which respect to digit count without using 
+       any mathematical library functions. */
+    for (; temp > 0; temp /= 10) {
+        int current_digit = temp % 10;
+        int current_power = 1;
+        for (int i = digit_count; i > 0; --i) {
+            current_power *= current_digit;
+        }
+        
+        sum += current_power;
+    }
+    
+    if (sum == number) {
+        printf("It is an Armstrong Number!\n");
+    } else {
+        printf("It is NOT an Armstrong Number!\n");
+    }
+
+    return 0;
 }
